d1064fdd8c3e9a43c6c2aa85fc5df213504528fc
sejmodha <sbm12uol@gmail.com>
2016-07-20 14:20:01 +0100

    pipeline updated to incorporate genome accession number in labels

--- CollectMetadata.sh
+++ CollectMetadata.sh
@@ -10,10 +10,16 @@ echo "ProteinAccession,TaxonomyID,SpeciesName,GenomeAccession,Lineage,Genus,URL"
 while read line
 do
 {
-	taxid=`elink -db protein -id "$line" -target taxonomy | efetch -format uid`
-	sciname=`elink -db protein -id "$line" -target taxonomy | esummary |xtract -element ScientificName`
-	lineage=`elink -db protein -id "$line" -target taxonomy | efetch -format xml|xtract -pattern TaxaSet -element Lineage|sed 's/; /;/g;s/, /-/g'`
-	genomeacc=`elink -db protein -id "$line" -target nuccore|efetch -format acc`
+	taxid=`elink -db protein -id "$line" -target taxonomy -batch| efetch -format uid`
+	sciname=`elink -db protein -id "$line" -target taxonomy -batch| esummary |xtract -element ScientificName`
+	lineage=`elink -db protein -id "$line" -target taxonomy -batch| efetch -format xml|xtract -pattern TaxaSet -element Lineage|sed 's/; /;/g;s/, /-/g'`
+
+	if [[ "$line" == *"_"*  ]];
+	then
+		genomeacc=`elink -db protein -id "$line" -target nuccore -batch |esummary| xtract -element AssemblyAcc`
+	else
+		genomeacc=`elink -db protein -id "$line" -target nuccore|efetch -format acc
+
 	genus=`echo $lineage|cut -f $3 -d";"`
 	echo $line,$taxid,$sciname,$genomeacc,$lineage,$genus,"http://www.ncbi.nlm.nih.gov/nuccore/"$genomeacc >> $2
 	
