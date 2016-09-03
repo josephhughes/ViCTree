29b28693764999a325bbddef1b092fd2f081b457
sejmodha <sbm12uol@gmail.com>
2016-09-03 19:16:27 +0100

    fixed the compatibility issue with eutils

--- CollectMetadata.sh
+++ CollectMetadata.sh
@@ -6,24 +6,28 @@
 #########################################################################################################
 
 touch $2
-echo "ProteinAccession_GenomeAcc,GenomeAcc_ScientificName,TaxonomyID,ScientificName,GenomeAccession,Lineage,Genus,URL" > $2
-while read line
+echo "ProteinAccession_GenomeAcc,GenomeAcc_ScientificName,TaxonomyID,ScientificName,Lineage,GenomeAccession,Genus,URL" > $2
+filename=$1
+IFS=$'\n'
+for line in `cat $filename`
 do
 {
-	taxid=`elink -db protein -id "$line" -target taxonomy -batch| efetch -format uid`
-	sciname=`elink -db protein -id "$line" -target taxonomy -batch| esummary |xtract -element ScientificName`
-	lineage=`elink -db protein -id "$line" -target taxonomy -batch| efetch -format xml|xtract -pattern TaxaSet -element Lineage|sed 's/; /;/g;s/, /-/g'`
-
+ 	taxonomy=`esearch -db protein -query "$line"|elink -target taxonomy |efetch -format xml|xtract -pattern Taxon -first TaxId ScientificName Lineage  -tab " "|sed 's/; /;/g;s/, /-/g;s/\t/,/g'`
+		
 	if [[ "$line" == *"_"*  ]];
 	then
-		genomeacc=`elink -db protein -id "$line" -target nuccore -batch |esummary| xtract -element AssemblyAcc`
+	
+		genomeacc=`elink -db protein -id "$line" -target nuccore -batch|efetch -format docsum|xtract -pattern DocumentSummary -element AssemblyAcc`
 	else
-		genomeacc=`elink -db protein -id "$line" -target nuccore|efetch -format acc`
+	
+		genomeacc=`elink -db protein -id "$line" -target nuccore -batch|efetch -format acc`
 	fi
 
-	genus=`echo $lineage|cut -f $3 -d";"`
-	echo $line"__"$genomeacc,$genomeacc"_"$sciname,$taxid,$sciname,$genomeacc,$lineage,$genus,"http://www.ncbi.nlm.nih.gov/nuccore/"$genomeacc >> $2
+	genus=`echo $taxonomy|cut -f $3 -d";"`
+	species=`echo $taxonomy|awk -F ";" '{print $NF}'`
+	sciname=`echo $taxonomy|cut -f2 -d ","`
+	echo $line"__"$genomeacc,$genomeacc"_"$sciname,$taxonomy,$genomeacc,$genus,$species,"http://www.ncbi.nlm.nih.gov/nuccore/"$genomeacc >> $2
 	
 	
 }
-done <$1
+done
